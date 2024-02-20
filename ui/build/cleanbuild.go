874110ff471e5484cc87be6924b39736e952baa2
jenslody <jens@jenslody.de>
2024-02-21 00:03:20 +0100

    cleanbuild: 0ur checksum files are named *.zip.sha256

--- ui/build/cleanbuild.go
+++ ui/build/cleanbuild.go
@@ -157,7 +157,7 @@ func installClean(ctx Context, config Config) {
 		productOut("*.img"),
 		productOut("*.json"),
 		productOut("*.zip"),
-		productOut("*.zip.sha256sum"),
+		productOut("*.zip.sha256"),
 		productOut("android-info.txt"),
 		productOut("misc_info.txt"),
 		productOut("apex"),
